0355df7dc4bdcbc563eb3efb89327b64269a7711
ολγα <@SHELDON>
2015-10-31 16:17:57 +0200

    νασδηαδωηακξδωη

--- Doors/src/Board.java
+++ Doors/src/Board.java
@@ -7,9 +7,14 @@ import java.util.Stack;
  *
  * **HashMap-like implementation for speed.
  * **Could be done more easily by having a "position" field in each piece but would hurt the speed.
- * 
+ * ------> oxi den 8a itan pio eukolo giati 8a diskoleue i anazitisi tou "posa poulia einai se mia 8esi"
+ * ------> outws i allws ta poulia dn exoun "noumero" opote dn se noiazei i 8esi enos sigkekrimenou pouliou
+ * ------> alla ta poulia se ka8e 8esi
  * For more extensibility we could create a class for the data structure and not just use ArrayList<Stack<Piece>>.
  * It looks kinda ugly.
+ * ------> nomizw enas pinakas einai arketos kai grigoros gia tis leitourgies pou 8eloume...xwro den pianei poli...
+ * ------> parola auta auto voleuei gia to grafiko kommati.....as skeftoume kai an ginetai alliws gia na diaxwrisoume
+ * ------> ilopoiisi me grafiki diepafi
  */
 public class Board {
 
